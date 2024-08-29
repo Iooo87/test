1dae8e77f4a704f0bf5a60f006ab0e23464e9b99
Iooo87 <103845860+Iooo87@users.noreply.github.com>
2024-08-29 12:11:10 +0300

    Update test.js

--- test.js
+++ test.js
@@ -87,7 +87,7 @@ const initializeZeroBounce = (config, form) => {
   const zb = new ZeroBounceApi(config.apiKey, disableSubmit, iframeDocument);
   const uniqueId = form[0].id.split('hsForm_')[1];
   console.log(uniqueId);
-  const inputs = iframeDocument.queryElement('[id="email_' + uniqueId + '"');
+  const inputs = iframeDocument.querySelectorAll('[id="email_' + uniqueId + '"');
   console.log(inputs);
   const loaderContainer = iframeDocument.createElement('div');
   const loader = iframeDocument.createElement('div');
