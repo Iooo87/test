e9d6224be07087de6b749ff581dc265dd7315146
Iooo87 <103845860+Iooo87@users.noreply.github.com>
2025-03-05 13:55:44 +0200

    Update test.js

--- test.js
+++ test.js
@@ -83,6 +83,8 @@ const initializeZeroBounce = (config) => {
   const selector = config.hubspotFormId.length > 0 ? `[id$='${config.hubspotFormId}'][type='email']` : '';
 
   const iframes = document.querySelectorAll("[id^='hs-form-iframe']");
+
+  console.log(iframes);
   
   if (iframes.length > 0) {
 
@@ -92,6 +94,8 @@ const initializeZeroBounce = (config) => {
     });
   } else {
     const form = document.getElementById(`[id^='hsForm_${config.hubspotFormId}']`);
+    console.log(form);
+    
     if (form) {
       processValidation(document, selector, disableSubmit, config.apiKey);
     }
