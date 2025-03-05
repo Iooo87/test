8de052f1a4f0bfacb985b2aa36394695ff34f5ba
Iooo87 <103845860+Iooo87@users.noreply.github.com>
2025-03-05 15:03:00 +0200

    Update test.js

--- test.js
+++ test.js
@@ -144,7 +144,7 @@ const initializeZeroBounce = (config) => {
       input.addEventListener('focus', function () {
         if (input.value.length > 0) {
           const parent = input.parentNode;
-          console.log(getComputedStyle(parent).width);
+          console.log(getComputedStyle(parent).width - getComputedStyle(loaderContainer).width);
           parent.style.position = 'relative';
           parent.insertBefore(loaderContainer, input.nextSibling);
         }
