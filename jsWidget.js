6d2e761ee7d33d0b4a3bde1f6d01e333a867ddd6
Iooo87 <103845860+Iooo87@users.noreply.github.com>
2025-03-18 11:16:13 +0200

    Update jsWidget.js

--- jsWidget.js
+++ jsWidget.js
@@ -11,106 +11,77 @@
         setupValidation: function () {
             console.log("Setting up email validation...");
             const inputs = document.querySelectorAll('.zb-email[type="email"]');
-            const loaderContainer = document.createElement('div');
-            const loader = document.createElement('div');
-            const logo = document.createElement('img');
             let delayTimer;
 
-            logo.src = 'https://www.zerobounce.net/cdn-cgi/image/fit=scale-down,format=auto,quality=100,height=23,metadata=none/static/logo.png';
-
-            loaderContainer.classList.add('loaderContainer');
-            loaderContainer.style.position = 'absolute';
-            loaderContainer.style.right = 0;
-            loaderContainer.style.borderRadius = '0 0 4px 4px';
-            loaderContainer.style.backgroundColor = '#fff';
-            loaderContainer.style.boxShadow = '0 2px 2px rgba(0,0,0,.2)';
-            loaderContainer.style.display = 'flex';
-            loaderContainer.style.alignItems = 'baseline';
-            loaderContainer.style.padding = '3px 5px 5px';
-            loaderContainer.style.height = '27px';
-            loaderContainer.style.border = '1px solid #bbbbbb';
-            loaderContainer.style.borderTop = 'none';
-            loaderContainer.style.zIndex = '1000';
-
-            loader.classList.add('loader');
-            loader.style.border = '3px solid';
-            loader.style.borderColor = '#888 #fbdd46 #888 #fbdd46';
-            loader.style.borderRadius = '50%';
-            loader.style.width = '10px';
-            loader.style.height = '10px';
-            loader.style.marginRight = '8px';
-
-            loader.animate([{ transform: 'rotate(0deg)' }, { transform: 'rotate(360deg)' }], {
-                duration: 2000,
-                iterations: Infinity,
-            });
-
-            loaderContainer.appendChild(logo);
-
             inputs.forEach((input) => {
-                input.addEventListener('focus', function () {
-                    const parent = input.parentNode;
-                    parent.insertBefore(loaderContainer, input.nextSibling);
-                });
+                const parent = input.parentNode;
 
-                input.addEventListener('blur', function () {
-                    const parent = input.parentNode;
-                    if (parent.querySelector('.loaderContainer')) {
-                        parent.removeChild(loaderContainer);
-                    }
-                });
+                if (ZBWidget.config.nonAcceptedStatusBehavior === "allow") {
+                    const hiddenInput = document.createElement('input');
+                    hiddenInput.type = 'hidden';
+                    hiddenInput.id = 'zbone_valid';
+                    parent.insertBefore(hiddenInput, input.nextSibling);
+                } else {
+                    if (ZBWidget.config.styling === "default") {
+                        const loader = document.createElement('div');
+                        loader.classList.add('loader');
+                        loader.style.border = '3px solid';
+                        loader.style.borderColor = '#888 #fbdd46 #888 #fbdd46';
+                        loader.style.borderRadius = '50%';
+                        loader.style.width = '10px';
+                        loader.style.height = '10px';
+                        loader.style.marginLeft = '5px';
+                        loader.animate([{ transform: 'rotate(0deg)' }, { transform: 'rotate(360deg)' }], {
+                            duration: 2000,
+                            iterations: Infinity,
+                        });
 
-                input.addEventListener('input', function (evt) {
-                    clearTimeout(delayTimer);
-                    const me = this;
-                    const parent = input.parentNode;
-                    const form = input.closest('form');
-                    const button = form.querySelector("[type='submit']");
-                    input.style.cssText = '';
-                    const inputStyles = window.getComputedStyle(input);
-                    const initBR = inputStyles.borderRadius;
-                    loaderContainer.style.borderColor = inputStyles.borderColor;
+                        input.addEventListener('focus', function () {
+                            parent.appendChild(loader);
+                        });
 
-                    if (input.classList.contains('zb-custom-error')) input.classList.remove('zb-custom-error');
-                    if (loaderContainer.classList.contains('zb-custom-error')) input.classList.remove('zb-custom-error');
+                        input.addEventListener('blur', function () {
+                            if (parent.contains(loader)) {
+                                parent.removeChild(loader);
+                            }
+                        });
 
-                    if (ZBWidget.config.disableSubmit && button) button.disabled = true;
-                    if (loaderContainer.querySelectorAll('.zb-icon').length > 0) {
-                        const icon = parent.querySelector('.zb-icon');
-                        loaderContainer.removeChild(icon);
-                    }
-                    if (me.value.length > 0) {
-                        parent.insertBefore(loaderContainer, input.nextSibling);
-                        input.style.borderRadius = initBR + ' ' + initBR + ' 0 ' + initBR;
-                    }
-
-                    loaderContainer.insertBefore(loader, loaderContainer.firstChild);
-                    delayTimer = setTimeout(function () {
-                        if (me.value === '' && parent.querySelectorAll('.loaderContainer').length > 0) {
-                            parent.removeChild(loaderContainer);
+                        input.addEventListener('input', function () {
+                            clearTimeout(delayTimer);
+                            const form = input.closest('form');
+                            const button = form.querySelector("[type='submit']");
                             input.style.cssText = '';
-                        }
-                        if (me.value !== '') ZBWidget.validate(me, loader, button, initBR);
-                    }, 1000);
-                });
+
+                            if (ZBWidget.config.disableSubmit && button) button.disabled = true;
+
+                            delayTimer = setTimeout(function () {
+                                if (input.value !== '') ZBWidget.validate(input, loader, button);
+                            }, 1000);
+                        });
+                    }
+                }
             });
         },
-        validate: function (input, loader, button, initBR) {
+
+        validate: function (input, loader, button) {
             const xhr = new XMLHttpRequest();
-            const uri = 'https://test-members-api.zerobounce.net/api/integration/widgets/validate/';
-            const container = loader.parentNode;
-            const iconContainer = document.createElement('div');
-            iconContainer.classList.add('zb-icon');
-            iconContainer.style.fontSize = '16px';
-            iconContainer.style.marginRight = '8px';
+            const uri = 'https://extension-api.zerobounce.net/api/integration/widgets/validate/';
+            const container = input.parentNode;
+            let messageContainer = container.querySelector('.zb-message');
+
+            if (!messageContainer) {
+                messageContainer = document.createElement('div');
+                messageContainer.classList.add('zb-message');
+                messageContainer.style.marginTop = '5px';
+                messageContainer.style.fontSize = '12px';
+                container.appendChild(messageContainer);
+            }
 
             const emailRegex = /^[a-zA-Z0-9._%+=!?/|{}$^~'`&#*-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
 
             if (!emailRegex.test(input.value)) {
-                container.removeChild(loader);
-                iconContainer.innerHTML = '&#x2718;';
-                iconContainer.style.color = '#DC143C';
-                container.insertBefore(iconContainer, container.firstChild);
+                messageContainer.innerHTML = ZBWidget.config.styling === "custom" ? ZBWidget.config.customStyling.invalidMessage : 'Invalid email format';
+                messageContainer.style.color = '#DC143C';
                 return;
             }
 
@@ -124,25 +95,15 @@
             xhr.setRequestHeader('Content-Type', 'application/json');
             xhr.send(jsonData);
 
-            container.removeChild(loader);
             const response = JSON.parse(xhr.response);
             if (xhr.readyState === 4 && xhr.status === 200) {
-                if (response.valid) {
-                    iconContainer.innerHTML = '&#x2713;';
-                    iconContainer.style.color = '#3cb043';
-                    iconContainer.style.transform = 'scale(1.5, 1)';
-                    if (ZBWidget.config.disableSubmit && button) {
-                        button.disabled = false;
-                    }
-                } else {
-                    iconContainer.innerHTML = '&#x2718;';
-                    iconContainer.style.color = '#DC143C';
-                    if (ZBWidget.config.disableSubmit) {
-                        input.style.borderColor = '#DC143C';
-                        container.style.borderColor = '#DC143C';
-                    }
+                messageContainer.innerHTML = response.valid
+                    ? (ZBWidget.config.styling === "custom" ? ZBWidget.config.customStyling.validMessage : 'Valid email')
+                    : (ZBWidget.config.styling === "custom" ? ZBWidget.config.customStyling.invalidMessage : 'Invalid email');
+                messageContainer.style.color = response.valid ? '#3cb043' : '#DC143C';
+                if (ZBWidget.config.disableSubmit && button) {
+                    button.disabled = !response.valid;
                 }
-                container.insertBefore(iconContainer, container.firstChild);
             }
         }
     };
