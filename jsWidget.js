7b31ad85b92969137ca6d00a45807e71cfd80608
Iooo87 <103845860+Iooo87@users.noreply.github.com>
2025-03-21 14:59:52 +0200

    Update jsWidget.js

--- jsWidget.js
+++ jsWidget.js
@@ -70,7 +70,7 @@
       },
       
       setupHiddenInput: function (parent, input) {
-        if (parent.querySelector("#zbone_valid") return;
+        if (parent.querySelector("#zbone_valid")) return;
         const hiddenInput = this.createElement('input');
         hiddenInput.type = 'hidden';
         hiddenInput.id = 'zbone_valid';
@@ -129,8 +129,8 @@
           public_key: this.config.apiKey, email: input.value, widget_type: 'js_widget'
         });
 
-        // const controller = new AbortController();
-        // const timeoutId = setTimeout(() => controller.abort(), this.config.timeoutLimit);
+        const controller = new AbortController();
+        const timeoutId = setTimeout(() => controller.abort(), this.config.timeoutLimit * 1000);
 
         const response = await fetch(uri, {
           method: 'POST',
@@ -139,7 +139,7 @@
             'Referer': this.config.domain
           },
           body: jsonData,
-          // signal: controller.signal
+          signal: controller.signal
         });
         const result = await response.json();
         const container = input.parentNode;
