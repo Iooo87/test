bfc7f09e11cf88205c8ec3f1abbeaa3c8317b425
Iooo87 <103845860+Iooo87@users.noreply.github.com>
2025-03-24 14:58:14 +0200

    Update jsWidget.js

--- jsWidget.js
+++ jsWidget.js
@@ -196,7 +196,7 @@
         this.setupSubmitButton(input, shouldBlock);
         if (this.config.timeoutLimitBehavior === 'block') {
           this.setupSubmitButton(input, true);
-          this.throwError(container, error.name === 'AbortError' : 'Request timed out': undefined);
+          this.throwError(container, error.name === 'AbortError' ? 'Request timed out': undefined);
         } else if (this.config.nonAcceptedStatusBehavior === 'block') {
           this.setupSubmitButton(input, true);
           this.throwError(container, undefined);
