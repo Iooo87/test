73d925e0e2102b3f6e4e989eeab6318c0e1947a2
Iooo87 <103845860+Iooo87@users.noreply.github.com>
2025-03-25 12:58:35 +0200

    Update jsWidget.js

--- jsWidget.js
+++ jsWidget.js
@@ -217,7 +217,7 @@
           container.appendChild(messageContainer);
         }
         messageContainer.id = htmlId;
-        messageContainer.innerHTML = invalidMessage || errorMessage;
+        messageContainer.innerHTML = errorMessage || invalidMessage;
         messageContainer.style.color = '#DC143C';
         messageContainer.style.display = 'block';
       } else {
